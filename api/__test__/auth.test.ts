2374e6b5ec508bf806444de6167728db5fac731f
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-01-18 15:45:41 +0300

    Add signin tests

--- api/__test__/auth.test.ts
+++ api/__test__/auth.test.ts
@@ -1,13 +1,22 @@
 import app from "../server";
 import * as testDB from "../config/dbTestConnect";
 import supertest from "supertest";
+import { faker } from "@faker-js/faker";
 
 const request = supertest(app);
 
-describe("Auth Signup controller", () => {
+const dummyUser = {
+  nickname: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+};
+
+describe("Auth Controllers", () => {
   beforeAll(async () => {
     await testDB.connect();
   });
+
+  //afterEach
 
   afterAll(async () => {
     await testDB.clearDatabase();
@@ -16,20 +25,14 @@ describe("Auth Signup controller", () => {
 
   test("POST - /signup --> When infos are missing. Return error message", async () => {
     const res = await request.post("/api/auth/signup").send({
-      password: "test.jest",
+      password: dummyUser.password,
     });
 
-    expect(res.body).toEqual(
-      expect.objectContaining({ success: false, message: "Missing info!" })
-    );
+    expect(res.body).toEqual({ success: false, message: "Missing info!" });
   });
 
   test("POST - /signup --> User is created successfully. Return success message", async () => {
-    const res = await request.post("/api/auth/signup").send({
-      nickname: "Test Jest",
-      email: "test.jest@gmail.com",
-      password: "test.jest",
-    });
+    const res = await request.post("/api/auth/signup").send(dummyUser);
 
     expect(res.body).toEqual(
       expect.objectContaining({
@@ -39,15 +42,41 @@ describe("Auth Signup controller", () => {
     );
   });
 
-  test("POST - /signup --> When email has already existed. Return success message", async () => {
-    const res = await request.post("/api/auth/signup").send({
-      nickname: "Test Jest",
-      email: "test.jest@gmail.com",
-      password: "test.jest",
+  test("POST - /signup --> When email has already existed. Return error message", async () => {
+    const res = await request.post("/api/auth/signup").send(dummyUser);
+
+    expect(res.body).toEqual({ success: false, message: "User already exist" });
+  });
+
+  test("POST - /signin --> When user doesn't exist. Return error message", async () => {
+    const res = await request.post("/api/auth/signin").send({
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+
+    expect(res.body).toEqual({ success: false, message: "User doesn't exist" });
+  });
+
+  test("POST - /signin --> When password isn't correct. Return error message", async () => {
+    const res = await request.post("/api/auth/signin").send({
+      email: dummyUser.email,
+      password: "incorrect password",
+    });
+
+    expect(res.body).toEqual({
+      success: false,
+      message: "Password is incorrect",
+    });
+  });
+
+  test("POST - /signin --> When signing is happend successfully. Return success message", async () => {
+    const res = await request.post("/api/auth/signin").send({
+      email: dummyUser.email,
+      password: dummyUser.password,
     });
 
     expect(res.body).toEqual(
-      expect.objectContaining({ success: false, message: "User already exist" })
+      expect.objectContaining({ success: true, message: "Signin Success!" })
     );
   });
 });
