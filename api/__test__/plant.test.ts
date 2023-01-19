86d319d5cc6edf9965017261b50c30d91dd20b89
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-01-19 09:28:45 +0300

    Add update plant controller in test

--- api/__test__/plant.test.ts
+++ api/__test__/plant.test.ts
@@ -5,7 +5,19 @@ import { faker } from "@faker-js/faker";
 
 const request = supertest(app);
 
+const dummyPlant = {
+  name: faker.animal.lion(),
+  petFriendly: faker.datatype.boolean(),
+  sunExposure: faker.datatype.number(),
+  fertilizer: faker.datatype.number(),
+  picture: faker.image.cats(),
+};
 const dummyUser = {
+  nickname: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+};
+const dummySecondUser = {
   nickname: faker.internet.userName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
@@ -14,6 +26,7 @@ const link = "/api/plant";
 
 describe("Plant Controllers", () => {
   let testToken: any;
+  let plantTestID: any;
   beforeAll(async () => {
     await testDB.connect();
 
@@ -23,6 +36,13 @@ describe("Plant Controllers", () => {
       .send({ email: dummyUser.email, password: dummyUser.password });
 
     testToken = signinRes.body.token;
+
+    const plantRes = await request
+      .post(`${link}/new`)
+      .set("authorization", testToken)
+      .send(dummyPlant);
+
+    plantTestID = plantRes.body.plant._id;
   });
 
   afterAll(async () => {
@@ -30,13 +50,32 @@ describe("Plant Controllers", () => {
     await testDB.closeDatabase();
   });
 
-  test("POST - /plant/new --> When authentication isn't exist. Return error message ", async () => {
-    const res = await request.post(`${link}/new`).send({
-      name: faker.animal.lion(),
-      petFriendly: faker.datatype.boolean(),
-      sunExposure: faker.datatype.number(),
-      fertilizer: faker.datatype.number(),
-      picture: faker.image.cats(),
+  test("POST - /plant/new --> When authentication isn't exist. Return error message", async () => {
+    const res = await request.post(`${link}/new`).send(dummyPlant);
+
+    expect(res.body).toEqual({
+      success: false,
+      message: "Invalid Authentication",
+    });
+  });
+
+  test("POST - /new --> When plant is successfully created. Return success message", async () => {
+    const res = await request
+      .post(`${link}/new`)
+      .set("authorization", testToken)
+      .send(dummyPlant);
+
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        success: true,
+        message: "Plant is successfully created!",
+      })
+    );
+  });
+
+  test("PUT - /update/:plantID --> When authentication isn't exist. Return error message", async () => {
+    const res = await request.put(`${link}/update/${plantTestID}`).send({
+      name: faker.animal.dog(),
     });
 
     expect(res.body).toEqual({
@@ -45,23 +84,37 @@ describe("Plant Controllers", () => {
     });
   });
 
-  test("POST - /plant/new --> When authentication isn't exist. Return error message ", async () => {
+  test("PUT - /update/:plantID --> When plant doesn't belong the user. Return error message", async () => {
+    await request.post("/api/auth/signup").send(dummySecondUser);
+    const newUsers = await request.post("/api/auth/signin").send({
+      email: dummySecondUser.email,
+      password: dummySecondUser.password,
+    });
+
     const res = await request
-      .post(`${link}/new`)
-      .set("authorization", testToken)
+      .put(`${link}/update/${plantTestID}`)
+      .set("authorization", newUsers.body.token)
       .send({
-        name: faker.animal.lion(),
-        petFriendly: faker.datatype.boolean(),
-        sunExposure: faker.datatype.number(),
-        fertilizer: faker.datatype.number(),
-        picture: faker.image.cats(),
+        name: faker.animal.dog(),
       });
 
-    expect(res.body).toEqual(
-      expect.objectContaining({
-        success: true,
-        message: "Plant is successfully created!",
-      })
-    );
+    expect(res.body).toEqual({
+      success: false,
+      message: "Plant doesn't belong the user!",
+    });
+  });
+
+  test("PUT - /update/:plantID --> When plant is successfully updated. Return success message", async () => {
+    const res = await request
+      .put(`${link}/update/${plantTestID}`)
+      .set("authorization", testToken)
+      .send({
+        name: faker.animal.dog(),
+      });
+
+    expect(res.body).toEqual({
+      success: true,
+      message: "Plant is successfully updated!",
+    });
   });
 });
