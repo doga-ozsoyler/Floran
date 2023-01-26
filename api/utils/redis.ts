2493079ecc81e2f5bd9ec801487feecdf6d7dff5
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-01-26 11:38:31 +0300

    Add redis mock because of jest error

--- api/utils/redis.ts
+++ api/utils/redis.ts
@@ -1,19 +1,8 @@
 import Redis from "ioredis";
-require("dotenv").config();
+import RedisTest from "ioredis-mock";
 
-const redisClient = new Redis();
-
-// function getOrSetCache(key: string, cb: () => void) {
-//   return new Promise((resolve, reject) => {
-//     redisClient.get(key, async (error: any, data: any) => {
-//       if (error) return reject(error);
-//       if (data != null) return resolve(JSON.parse(data));
-//       const freshData = await cb();
-//       redisClient.setex(key, 60, JSON.stringify(freshData));
-//       resolve(freshData);
-//     });
-//   });
-// }
+const redisClient =
+  process.env.NODE_ENV !== "TEST" ? new Redis() : new RedisTest();
 
 export const getOrSetCache = async (
   key: string,
