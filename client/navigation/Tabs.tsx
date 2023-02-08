28bead033b8df8334173370aeab394d91addadf5
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-02-08 16:18:29 +0300

    Add profile page

--- client/navigation/Tabs.tsx
+++ client/navigation/Tabs.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AllPlantsScreen from "../screens/AllPlantsScreen";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import Profile from "../screens/Profile";
 
 const Tabs = () => {
   const Tab = createBottomTabNavigator();
@@ -22,8 +23,26 @@ const Tabs = () => {
             fontWeight: "bold",
           },
         }}
-        name="Home"
+        name="AllPlantsScreen"
         component={AllPlantsScreen}
+      />
+      <Tab.Screen
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name={focused ? "user-alt" : "user"}
+              color="green"
+              size={30}
+            />
+          ),
+          headerTintColor: "green",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+        name="Profile"
+        component={Profile}
       />
     </Tab.Navigator>
   );
---
+++ client/screens/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Center, Text } from "native-base";
+
+const Profile = () => {
+  return (
+    <Center flex={1}>
+      <Text>Profile</Text>
+    </Center>
+  );
+};
+
+export default Profile;
