817142a47cfba1818650af727c537574308543fb
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-02-21 17:23:11 +0300

    Add own plant reducer

--- client/redux/slices/userReducer.ts
+++ client/redux/slices/userReducer.ts
@@ -1,9 +1,10 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { UserRes, UserState } from "../types";
 import { RootState } from "../store";
 
 const SERVER_URL = "http://192.168.1.2:3939/api";
+const updatedUser = createAction("user/update");
 
 export const fetchUser = createAsyncThunk<UserRes>(
   "user/fetchUser",
@@ -25,6 +26,34 @@ export const fetchUser = createAsyncThunk<UserRes>(
   }
 );
 
+export const ownPlant = createAsyncThunk(
+  "user/ownPlant",
+  async (plantID: string, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const state = getState() as RootState;
+      const token = state.auth.token;
+
+      const { data } = await axios.put(
+        `${SERVER_URL}/user/own/plants`,
+        { plantID: plantID },
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      console.log("here");
+
+      dispatch(updatedUser());
+      return data;
+    } catch (error) {
+      console.log("error");
+      console.log(error);
+      return rejectWithValue(error);
+    }
+  }
+);
+
 const initialState = {
   loading: false,
   error: null,
@@ -37,6 +66,9 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(updatedUser, (state) => {
+      state.isUpdated = true;
+    });
     builder.addCase(fetchUser.pending, (state) => {
       state.loading = true;
     });
@@ -48,6 +80,20 @@ const userSlice = createSlice({
     builder.addCase(fetchUser.rejected, (state, action) => {
       state.loading = false;
       state.error = action?.error;
+    });
+    builder.addCase(ownPlant.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(ownPlant.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = null;
+      state.userRes = action?.payload;
+      state.isUpdated = false;
+    });
+    builder.addCase(ownPlant.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action?.error;
+      state.userRes = null;
     });
   },
 });
