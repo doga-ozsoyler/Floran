e33617492b9be7919e11bbbbf79b759001801e6f
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-04 13:06:03 +0300

    changed AddPlantScreen a bit

--- client/screens/AddPlantScreen.tsx
+++ client/screens/AddPlantScreen.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Center, Text } from "native-base";
+import React, { useState } from "react";
+import { Center, HStack, Text, Switch } from "native-base";
+import FormController from "../components/InputFormController";
 
 const AddPlantScreen = () => {
+  const [plantName, setPlantName] = useState<string>("");
+
   return (
     <Center flex={1}>
-      <Text>Add Plant</Text>
+      <FormController
+        label="Plant Name"
+        message={"userError?.message"}
+        errorMessageShow={false}
+        value={plantName}
+        onChangeText={setPlantName}
+      />
+      <HStack width="70%" alignItems="center" justifyContent="space-between">
+        <Text>Pet Friendly</Text>
+        <Switch
+          size="sm"
+          colorScheme="emerald"
+          onToggle={() => console.log("checked")}
+        />
+      </HStack>
     </Center>
   );
 };
